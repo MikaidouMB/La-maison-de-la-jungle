b7d2a25638f09a89e3e546a2bee316c3be199241
mikaidou <mikaidoumbo@hotmail.fr>
2023-11-15 18:05:41 +0100

    add comp shoppingList- cat fc and if

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,18 @@
+import './../styles/Cart.css'
+
+
 function Cart() {
     let monsteraPrice =  8;
     let lierrePrice = 10 ;              
     let fleursPrice = 15 ;
     let total = monsteraPrice + lierrePrice + fleursPrice;
      return(
-       <div>
+       <div className="lmj-cart">
         <h2>Panier</h2>
-            <ul>
-              <li>monstera : {monsteraPrice} € </li>
-              <li>lierre : {lierrePrice} €</li>
-              <li>fleurs : {fleursPrice} €</li>
-             </ul>
-            <p>prix total : {total} €</p>
+              monstera : {monsteraPrice} € <br></br>
+              lierre : {lierrePrice} €<br></br>
+              fleurs : {fleursPrice} €<br></br>
+            <p className='total-cart'>prix total : {total} €</p>
        </div>
          ) 
      }
